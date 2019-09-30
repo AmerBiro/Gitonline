fc0afc8b61b5cfaafa4208a1c80e84f5364330a8
soleimani1984 <z.soleimani1984@gmail.com>
2019-09-30 15:15:41 +0200

    vi lærer git en dag

--- src/Class_New.java
+++ src/Class_New.java
@@ -1,5 +1,7 @@
 public class Class_New {
     public static void main(String[] args) {
         System.out.println("Welcome to the new fucking Git!");
+        System.out.println("vi skal nok lære git");
     }
+
 }
